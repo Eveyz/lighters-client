d452f7ad00bbc266477daa94aa322a5bdc0b7b53
Ningzhou Zeng <nxz84@case.edu>
2019-04-24 10:35:16 -0500

    fix token expiration time

--- src/components/auth/requireAuth.js
+++ src/components/auth/requireAuth.js
@@ -1,8 +1,5 @@
 import React from 'react';
-
 import { Route, Redirect } from 'react-router-dom';
-
-var dateNow = new Date();
 
 export const PrivateRoute = ({ component: Component, auth, ...rest }) => (
   <Route {...rest} render={(props) => (
@@ -13,28 +10,31 @@ export const PrivateRoute = ({ component: Component, auth, ...rest }) => (
 )
 
 export const AdminRoute = ({component: Component, auth, ...rest}) => (
-  <Route {...rest} render={(props) => (
-    (auth.isAuthenticated && auth.user.userTokenData.identity === "admin") 
-    ? auth.user.exp > (dateNow.getTime()/1000) 
+  <Route {...rest} render={(props) => {
+    const dateNow = new Date()
+    return auth.isAuthenticated && auth.user.userTokenData.identity === "admin" 
+    ? auth.user.exp >= (dateNow.getTime()/1000) 
     ? <Component {...props} /> : <Redirect to='/login' /> 
     : <Redirect to='/' />
-  )} />
+  }} />
 )
 
 export const TeacherRoute = ({component: Component, auth, ...rest}) => (
-  <Route {...rest} render={(props) => (
-    (auth.isAuthenticated && auth.user.userTokenData.identity === "teacher") 
-    ? auth.user.exp > (dateNow.getTime()/1000) 
+  <Route {...rest} render={(props) => {
+    const dateNow = new Date()
+    return auth.isAuthenticated && auth.user.userTokenData.identity === "teacher" 
+    ? auth.user.exp >= (dateNow.getTime()/1000) 
     ? <Component {...props} /> : <Redirect to='/login' /> 
     : <Redirect to='/' />
-  )} />
+  }} />
 )
 
 export const StudentRoute = ({component: Component, auth, ...rest}) => (
-  <Route {...rest} render={(props) => (
-    (auth.isAuthenticated && auth.user.userTokenData.identity === "student") 
-    ? auth.user.exp > (dateNow.getTime()/1000) 
+  <Route {...rest} render={(props) => {
+    const dateNow = new Date()
+    return auth.isAuthenticated && auth.user.userTokenData.identity === "student" 
+    ? auth.user.exp >= (dateNow.getTime()/1000) 
     ? <Component {...props} /> : <Redirect to='/login' /> 
     : <Redirect to='/' />
-  )} />
+  }} />
 )
