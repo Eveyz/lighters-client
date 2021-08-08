a082a38f6cef5c3e9ff0953a8469bfe9d43a35eb
Ningzhou Zeng <znz@Ningzhous-MacBook-Pro.local>
2021-08-08 14:34:22 +0800

    fixed bugs

--- src/components/evaluations/EvaluationForm.js
+++ src/components/evaluations/EvaluationForm.js
@@ -21,11 +21,15 @@ const EvaluationForm = props => {
 
 	const handleSubmit = (e) => {
     e.preventDefault();
+		var _content = content
+		if(props.action === "EDIT" && content === "") {
+			_content = props.evaluation.content
+		}
     const evaluation = {
       name: nameInput.current.value,
       level: levelInput.current.value,
       date: dateInput.current.value,
-      content: content,
+      content: _content,
 			student_id: props.student_id
     };
 		props.handleSubmit(evaluation);
@@ -49,15 +53,15 @@ const EvaluationForm = props => {
 				<div>
 					<div className='row'>
 						<div className="col input-field s4">
-							<input type="date" defaultValue={getLocalDateFormat(props.evaluation.date) || ""} ref={dateInput} id="date" />
+							<input type="date" defaultValue={props.action === "ADD" ? "" : getLocalDateFormat(props.evaluation.date)} ref={dateInput} id="date" />
 							<label htmlFor="date">评测日期</label>
 						</div>
 						<div className="col input-field s4">
-							<input type="text" defaultValue={props.evaluation.level || ""} ref={levelInput} id="level" />
+							<input type="text" defaultValue={props.action === "ADD" ? "" : props.evaluation.level} ref={levelInput} id="level" />
 							<label htmlFor="level">评测等级</label>
 						</div>
 						<div className="col input-field s4">
-							<input type="text" defaultValue={nameVal} ref={nameInput} id="name" />
+							<input type="text" defaultValue={props.action === "ADD" ? "" : props.evaluation.name} ref={nameInput} id="name" />
 							<label htmlFor="name">评测人</label>
 						</div>
 					</div>
@@ -69,7 +73,7 @@ const EvaluationForm = props => {
 						errorMsg="请填写评语"
 						getInputData={getInputData}
 						action={props.action}
-						value={props.action === "EDIT" ? props.evaluation.content : ""}
+						value={props.action === "ADD" ? "" : props.evaluation.content}
 					/>
 					<button className="btn" onClick={handleSubmit}>提交</button>
 					<button onClick={handleCancel} className="btn white black-text right">取消</button>
